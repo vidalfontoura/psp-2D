d6d8a46f95053455ae86e6d50d3cb3dc74d747de
Vidal Daniel da Fontoura <vdfontoura@inf.ufpr.br>
2016-01-11 17:27:59 -0200

    Changes on script

---
+++ cbic_approach/scripts/sq1/IBEA.sh
@@ -0,0 +1,12 @@
+java -jar ../../target/IBEA.jar \
+				100 \
+				25000 \
+				SinglePointCrossover \
+				BitFlipMutation \
+				200 \
+				0.9 \
+				0.01 \
+				30 \
+				20 \
+				results \
+				HPHPPHHPHHPHPHHPPHPH &
---
+++ cbic_approach/scripts/sq1/NSGAII.sh
@@ -0,0 +1,12 @@
+java -jar ../../target/NSGAII.jar \
+				100 \
+				25000 \
+				SinglePointCrossover \
+				BitFlipMutation \
+				0 \
+				0.9 \
+				0.01 \
+				30 \
+				15 \
+				results \
+				HPHPPHHPHHPHPHHPPHPH &
