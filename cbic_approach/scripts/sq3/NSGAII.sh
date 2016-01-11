5b26c810ae9fcbfce6a3a851052457655d3875dc
Vidal Daniel <vfontoura@kenzan.com>
2016-01-11 17:39:41 -0200

    Fixing scripts

---
+++ cbic_approach/scripts/sq3/NSGAII.sh
@@ -0,0 +1,12 @@
+java -jar ../../target/NSGAII.jar \
+				500 \
+				250000 \
+				SinglePointCrossover \
+				BitFlipMutation \
+				0 \
+				0.9 \
+				0.01 \
+				30 \
+				1 \
+				results \
+				PPHPPHHPPPPHHPPPPHHPPPPHH &
